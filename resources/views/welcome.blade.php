f57136d2d7da3610912399dd1386de468c44e205
Adrien Maranville <Adrien@Maranville.IO>
2019-01-02 21:39:29 -0700

    adding more changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,12 +85,12 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://laravel.com/docs">Forge</a>
+                    <a href="https://laracasts.com">Forge</a>
+                    <a href="https://laravel-news.com">Forge</a>
+                    <a href="https://nova.laravel.com">Forge</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">Forge</a>
                 </div>
             </div>
         </div>
